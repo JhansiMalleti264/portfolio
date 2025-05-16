03799f48987c0e0976ad219652693e16c1807673
JhansiMalleti264 <jhansimalleti654@gmail.com>
2025-05-16 13:10:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.getElementById('contact-form').addEventListener('submit', (e) => {
   alert('Message sent successfully!');
   e.target.reset();
 });*/
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzMSrHzqsI1rTDFqqEC7mnOmryuHY82Lb0ypw8c8jiG7nvi96IId6h28NS0iLPoO_wtbA/exec'; // Replace with your deployment URL
+const scriptURL = 'https://script.google.com/macros/library/d/1hW--Y2XBBDm_SbmZ4BpI6fEll4KiQ107ggS6IW3sdBvGcJyINJ3LwIax/1'; // Replace with your deployment URL
 
   document.getElementById('contact-form').addEventListener('submit', e => {
     e.preventDefault();
